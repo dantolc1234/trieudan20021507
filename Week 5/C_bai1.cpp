5041a4479f0b4efbbf7da6a4ee71ec00bd557d98
TrieuDan <crazysub2002@gmail.com>
2021-03-18 20:18:16 +0700

    em khong lam dc bai 1 phan C :((

--- Week 5/C_bai1.cpp
+++ Week 5/C_bai1.cpp
@@ -1,17 +1,42 @@
 #include <iostream>
+#include <stdlib.h>
 
-using namespace std; 
+using namespace std;
 
-// void taoMap(int m, int n, int k)
-// {
+int minum; // tạo mìn
 
-// }
+void taoMap(int m, int n)
+{
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+}
+
+void taoMin(int m, int n)
+{
+    int i, j;
+    time_t t;
+    int r;
+    int row, col;
+    int iminum = 0;
+    srand((unsigned) time(&t));
+    while (iminum < minum) {
+    r = rand() % ((m + 1) * (n + 1));
+    row = (r - 1) / (n + 1);
+    col = r - row * (m + 1) - 1;
+    }
+}
 
 int main()
 {
     int m, n, K;
     cin >> m >> n >> K;
-    int * array[m][n];
+    taoMap(m, n);
+
+    int array[m][n];
 
     return 0;
 }
