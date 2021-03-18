62b7e9a3964d9ed36e61fbcfdf5092e6beb52625
TrieuDan <crazysub2002@gmail.com>
2021-03-18 19:18:03 +0700

    bo sung c

---
+++ Week 5/C_bai1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std; 
+
+// void taoMap(int m, int n, int k)
+// {
+
+// }
+
+int main()
+{
+    int m, n, K;
+    cin >> m >> n >> K;
+    int * array[m][n];
+
+    return 0;
+}
