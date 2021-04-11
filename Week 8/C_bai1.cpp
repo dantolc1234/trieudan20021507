1a27e0ef14f8866c8dac48594c9676fc58a856e1
TrieuDan <crazysub2002@gmail.com>
2021-04-11 21:44:31 +0700

    add w8 C_bai1

--- Week 8/C_bai1.cpp
+++ Week 8/C_bai1.cpp
@@ -1,27 +1,92 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
 void reverse(char a[])
 {
-    int cout = 0;
-	while (a[cout] != '\0') cout++;
-    char temp[cout];
-	for (int i = 0; i < cout; i++)
+    int count = 0;
+	while (a[count] != '\0') count++;
+    char * temp = new char[count + 1];
+    //char temp[count + 1];
+	for (int i = 0; i < count; i++)
 	{
-		 temp[i] = a[cout - 1 - i];
+		 *(temp + i) = a[count - 1 - i];
 	}
-    for (int i = cout - 1; i >= 0; i--)
-    {
-        cout << temp[i];
-    }
+	temp[count] = '\0';
+	for(int i = 0; i < count; i++){
+		cout << *(temp + i);
+	}
+}
+void delete_char(char a[], char c)
+{
+	int count = 0;
+	while (a[count] != '\0') count++;
+	for(int i = 0; i < count; i++){
+		if(a[i] != c) cout << a[i];
+	}
+}
+// void pad_right(char * s, int n)
+// {
+// 	int count = 0;
+// 	while (*(s + count) != '\0') count++;
+// 	if (count < n) {for (int i = count; i < n; i++) *(s + i) = '_'; *(s + n) = '\0';}
+// 	for(int i = 0; i < n; i++){
+// 		cout << *(s + i);
+// 	}
+// }
+void truncate(char a[], int n)
+{	
+	int count = 0;
+	while (a[count] != '\0') count++;
+	if(count > n) {for (int i = 0; i < n; i++) {
+		cout << a[i];
+		}
+	}
+}
+// bool is_palindrome(char a[])
+// {
+// 	int count = 0;
+// 	while(a[count] != '\0') count++;
+// 	char * temp = new char[count + 1];
+// 	for(int i = 0; i < count; i++){
+// 		*(temp + i) = a[i];
+// 	}
+// 	temp[count] = '\0';
+// 	for(int i = 0; i < count; i++) {
+// 		for(int j = count - 1; i >= 0; i--){
+// 		if(a[i] != *(temp + j)) return false;
+// 		else return true;
+// 		}
+// 	}
+// }
+void trim_left(char a[])
+{
+	int count;
+	while(a[count] != '\0') count++;
+	for(int i = 0; i < count; i++){
+		if(a[i] != '_') cout << a[i];
+	}
 }
 
 int main()
 {
     char a[100];
+	char * s = a;
+    char c = 'a';
     cout << "Moi nhap xau a:";
     fgets(a, 100, stdin);
-    reverse(a);
+	reverse(a);
+    cout << endl;
+    delete_char(a, c);
+    cout << endl;
+    // pad_right(s, 15);
+	// cout << endl;
+	truncate(a, 10);
+	cout << endl;
+	// if(is_palindrome(a)) cout << "Xau tren la xau doi xung";
+	// else cout << "Xau tren khong doi xung";
+	trim_left(a); 
+
     return 0;
 }
