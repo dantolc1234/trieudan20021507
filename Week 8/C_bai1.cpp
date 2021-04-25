406d2a5005f0a781f78f87cac465b5c5b22f07fc
TrieuDan <crazysub2002@gmail.com>
2021-04-25 19:36:22 +0700

    fix w8c1

--- Week 8/C_bai1.cpp
+++ Week 8/C_bai1.cpp
@@ -26,15 +26,17 @@ void delete_char(char a[], char c)
 		if(a[i] != c) cout << a[i];
 	}
 }
-// void pad_right(char * s, int n)
-// {
-// 	int count = 0;
-// 	while (*(s + count) != '\0') count++;
-// 	if (count < n) {for (int i = count; i < n; i++) *(s + i) = '_'; *(s + n) = '\0';}
-// 	for(int i = 0; i < n; i++){
-// 		cout << *(s + i);
-// 	}
-// }
+ void pad_right(char * s, int n)
+{
+ 	int count = 0;
+ 	while (*(s + count) != '\0') count++;
+ 	if (count < n) {
+		for (int i = count - 1; i <= n; i++) 
+			*(s + i) = '_'; 
+		*(s + n) = '\0';	
+	}
+ 	cout << s;
+}	
 void truncate(char a[], int n)
 {	
 	int count = 0;
@@ -44,22 +46,22 @@ void truncate(char a[], int n)
 		}
 	}
 }
-// bool is_palindrome(char a[])
-// {
-// 	int count = 0;
-// 	while(a[count] != '\0') count++;
-// 	char * temp = new char[count + 1];
-// 	for(int i = 0; i < count; i++){
-// 		*(temp + i) = a[i];
-// 	}
-// 	temp[count] = '\0';
-// 	for(int i = 0; i < count; i++) {
-// 		for(int j = count - 1; i >= 0; i--){
-// 		if(a[i] != *(temp + j)) return false;
-// 		else return true;
-// 		}
-// 	}
-// }
+bool is_palindrome(char a[])
+{
+	int count = 0;
+	while(a[count] != '\0') count++;
+	char * temp = new char[count + 1];
+	for(int i = 0; i < count; i++){
+		*(temp + i) = a[i];
+	}
+	temp[count] = '\0';
+	for(int i = 0; i < count; i++) {
+		for(int j = count - 1; i >= 0; i--){
+		if(a[i] != *(temp + j)) return false;
+		else return true;
+		}
+	}
+}
 void trim_left(char a[])
 {
 	int count;
@@ -75,18 +77,18 @@ int main()
 	char * s = a;
     char c = 'a';
     cout << "Moi nhap xau a:";
-    fgets(a, 100, stdin);
+    fgets(a, 100, stdin); // lay ca dau enter, nen dung ham getline.
 	reverse(a);
     cout << endl;
     delete_char(a, c);
     cout << endl;
-    // pad_right(s, 15);
-	// cout << endl;
+    pad_right(s, 15);
+	cout << endl;
 	truncate(a, 10);
 	cout << endl;
-	// if(is_palindrome(a)) cout << "Xau tren la xau doi xung";
-	// else cout << "Xau tren khong doi xung";
-	trim_left(a); 
+	if(is_palindrome(a)) cout << "Xau tren la xau doi xung";
+	else cout << "Xau tren khong doi xung";
+	trim_left(a);
 
     return 0;
 }
